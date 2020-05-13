477bd3f7f653513cfbbc2966b60871e5a00cca05
AnatolyOsovitny <anatoly.osovitny@outlook.com>
2020-05-14 01:33:51 +0700

    page title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [Title]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = "Angular";
+  constructor(private title: Title) {
+    this.title.setTitle("page title changed");
+  }
 }
